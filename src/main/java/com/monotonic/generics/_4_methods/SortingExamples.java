a9b5c16a98eee864b63ce2401077ee6268a6eab8
Julie Java <juliejava@icloud.com>
2019-02-27 16:57:31 -0600

    Completed Generics on Methods Tutorial in Pluralsight on 2/27/2019

--- src/main/java/com/monotonic/generics/_4_methods/SortingExamples.java
+++ src/main/java/com/monotonic/generics/_4_methods/SortingExamples.java
@@ -17,10 +17,66 @@ public class SortingExamples
         madMen.add(peggyOlson);
         madMen.add(bertCooper);
 
+        // have to cast this as a Person cuz it just resturns Object
+        final Person youngestCastMember2 =
+                (Person) min2(madMen, new AgeComparator());
+
+        System.out.println();
+        System.out.println("*** With min2 method ***");
+        System.out.println(youngestCastMember2);
+
         final Person youngestCastMember = min(madMen, new AgeComparator());
 
+        System.out.println();
+        System.out.println("*** With min method ***");
         System.out.println(youngestCastMember);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        System.out.println();
+        System.out.println("*** Now min with Integers ***");
+
+        //did this one myself!!!!
+        final int minNum = min(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
+
+        System.out.println(minNum);
+
+        //code from tutorial:
+        System.out.println();
+        System.out.println("*** Now min with Integers Using Tutorial Code ***");
+        System.out.println(min(numbers, Integer::compare));
+
     }
+
+
+    // write min in the generic way -- by name (string) or age (integer)
+
+    public static Object min2(List values, Comparator comparator) {
+        if (values.isEmpty()){
+            throw new IllegalArgumentException("List is empty, cannot find minimum");
+        }
+        // then loop over the list of values and incrementally find the lowest element
+        // then return lowest element
+        Object lowestElement = values.get(0);
+
+        for (int i = 1; i < values.size(); i++){
+            final Object element = values.get(i);
+
+            if (comparator.compare(element, lowestElement) < 0){
+                lowestElement = element;
+            }
+        }
+        return lowestElement;
+    }
+
 
     public static <T> T min(List<T> values, Comparator<T> comparator)
     {
